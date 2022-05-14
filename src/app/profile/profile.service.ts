afe28e83640784c2cc53abdbf600845b2a6b67ce
Breno Fernandes <brenojesusfernandes@pm.com>
2022-05-13 22:45:31 -0230

    Update content with my info

--- src/app/profile/profile.service.ts
+++ src/app/profile/profile.service.ts
@@ -181,12 +181,12 @@ export class ProfileService {
     },
 
   ]
-  about2 = `Software Developer with 2.5+ years of experience .
-  Worked with Software Development Life Cycle(SDLC) and  agile methodologies .
-  Expertise in Angular 2+ All versions, React, Javascript,  Node JS and UI with Responsive Designs .
-  Have good understanding of Python, Jquery, Bootstrap, MongoDB, Firebase, and MYSQL`
+  about2 = `Software Enginner with 2.0+ years of experience .
+  AWS and C# Certificated.
+  Expertise in Python, SQL, C# and Angular.
+  Have good understanding of `
 
-  about = "Lover of innovation and everything related to generate new knowledge.Face problems with a smile and solve them as soon as possible. Very calculated about the time I spend and work I do."
+  about = "Lover of innovation and everything related to generate new solutions. Face problems with a smile and solve them as soon as possible."
   resumeurl = "https://drive.google.com/file/d/1XY0xlYDX8aZA1KwCnnjH6Y3G4iTWcV-D/view?usp=sharing"
 
   skillsData: any = [
@@ -257,54 +257,24 @@ Completed my Secondary school with 84%.`
   ];
   exprienceData: any = [
     {
-      id: 4,
-      company: 'Crest Data System',
-      location: 'Ahmedabad',
-      timeline: 'Aug 2021 to Present',
-      role: 'Software Developer',
-      work: 'Working as a full stack developer .Responsible for handling the UI in React and Managing the api calls in python Flask.'
+      id: 2,
+      company: 'Itau Unibanco',
+      location: 'Sao Paulo',
+      timeline: 'Set 2020 to Present',
+      role: 'Data Scientist',
+      work: 'Working as a data scientist. Responsible for handling the UI in React and Managing the api calls in python Flask.'
 
     },
     {
-      id: 3,
-      company: 'Indianic Info Tech',
-      location: 'Ahmedabad',
-      timeline: 'Feb 2021 to June 2021',
-      role: 'Software Developer',
+      id: 1,
+      company: 'Evo Systems',
+      location: 'Sao Paulo',
+      timeline: 'Aug 2018 to Aug 2020',
+      role: 'Software Enginner',
       work: `Technologies : Angular , Node JS , MongoDB . 
       Worked on the web application built in Angular for different business and Campaigns . 
       Contributed on developing the admin dashboard with different charts and insights for the application.`
-    },
-
-    {
-      id: 2,
-      company: 'Growth99',
-      location: 'Remote',
-      timeline: '',
-      role: 'Frontend  Developer',
-      work: `Technologies : Angular , Node JS , MongoDB . 
-  Worked on the product of Growth99 which allowed other clients to come and integrate our product in their website . 
-  Worked on REST Api , Admin Dashboard , Appointment Booking for services and UI of the application . 
-  Created sections like templates for email, sms, landing pages, clinic, services, providers and triggers Automation. 
-  `
-    },
-
-    {
-      id: 1,
-      company: 'Tata Consultancy Services',
-      location: 'Gandhinagar and Pune',
-      timeline: 'June 2019 - Jan 2021',
-      role: 'Assistant System Engineer Trainee',
-      work: `Technologies : Angular , JavaSpring Boot , SQL. Apache Solr . 
-  
-  Project 1 : Worked on uploading two excel sheets with bulk of data and finding the matches for each business . 
-  
-  Project 2 : Worked on indexing the search results inside the application with bulk of data using Apache Solr and configuring the server with cron jobs and shell scripts . 
-  
-  Worked on ranking the search results based on various criteria,features and delivering the accurate results based on generated scores.
-  `
-    },
-
+    }
 
   ]
   // contactus(data: any): Observable<any> {
